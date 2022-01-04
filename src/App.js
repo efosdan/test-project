a5976626f0772c158678f1c8d863d91ae04ddd91
efosdan <efosdan@yahoo.com>
2022-01-05 00:14:23 +0300

    added Footed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div>
         <h1>Hello World people!</h1>
       </div>
+      <Footer />
     </div>
   );
 }
