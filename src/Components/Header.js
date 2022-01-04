f216cf5b4d8b5533b6f8f33d90821a80537969a6
efosdan <efosdan@yahoo.com>
2022-01-05 00:01:56 +0300

    Added header and modifeied app.js

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>new paragraph</p>
+      <p> this is another para</p>
+    </div>
+  );
+}
+
+export default Header;
